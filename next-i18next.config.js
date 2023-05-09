b646a3fcb9b86b5c511e4d4a728123c83f78455f
Salih <salihozdemir94@gmail.com>
2023-05-09 17:20:30 +0300

    chore: update the i18n config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,9 +1,9 @@
 const path = require("path");
 
 module.exports = {
-    i18n: {
-        defaultLocale: "en",
-        locales: ["en", "de"],
-        localePath: path.resolve("./public/locales"),
-    },
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "de"],
+  },
+  localePath: path.resolve("./public/locales"),
 };
